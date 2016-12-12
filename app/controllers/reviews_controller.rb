14108f3d4974b007de060fc3f6a0119b065f4186
Kelly James <kellyjamesclancy@gmail.com>
2016-12-12 19:13:48 +0000

    Product rating feature functioning properly

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,18 @@
 class ReviewsController < ApplicationController
 
+  before_filter :only => [:show]
+
   def create
     product = Product.find(params[:product_id])
     review = product.reviews.new(review_params)
     review.user_id = current_user.id
     review.save
-    # redirect_to "/products/#{product.id}"
     redirect_to product_path(product.id)
-    # binding.pry
   end
 
   def destroy
+    Review.find(params[:id]).destroy
+    redirect_to product_path(params[:product_id])
   end
 
   private
